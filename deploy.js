7137eeeb44ffd997834423d5db585c49d1bbdd4c
susingh <aimsushmitasingh@gmail.com>
2025-01-04 14:45:02 +0530

    Deploy React App

--- deploy.js
+++ deploy.js
@@ -1,6 +1,10 @@
 const ghpages = require('gh-pages');
+const path = require('path');
 
-ghpages.publish('build', function(err) {
+const buildPath = path.join(__dirname, 'build');
+const tempPath = path.join('C:\List-Creation', 'gh-pages-temp');
+
+ghpages.publish(buildPath, { dotfiles: true, temp: tempPath }, function(err) {
   if (err) {
     console.error('Deployment failed:', err);
   } else {
